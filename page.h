80e9492834f7dcede43da5ea0182b65f19af99fd
ms2022-16686 <a3005harry@snu.ac.kr>
2023-05-08 15:05:48 +0900

    find page #1

--- page.h
+++ page.h
@@ -1,6 +1,11 @@
 class Page {
     public:
+        Page(int in_x,int in_y,int in_width,int in_height,int in_id,char in_content):x(in_x),y(in_y),width(in_width),height(in_height),id(in_id),content(in_content){
 
+        }
+        static int Page::find_page(int in_id){
+            
+        }
     private:
         int x, y; // position of the page on the board
         int width, height; // width and height of the page 
