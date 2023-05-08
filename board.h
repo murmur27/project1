f42c6aaa87177269220ae83d35bb263e768ac17e
ms2022-16686 <a3005harry@snu.ac.kr>
2023-05-08 22:47:13 +0900

    output #1

--- board.h
+++ board.h
@@ -84,23 +84,24 @@ void Board::print_job(int job_idx, char job_type, int id) {
 
 
 void Board::insert_page(int x, int y, int width, int height, int id, char content) {
+    Page page(x,y,width,height,id,content);
     for (int h = y; h < height; h++) {
         for (int w = x; w < width; w++) {
             board[h*width + w] = content;
         }
     }
-    print_board;
+    print_board();
 }
 
 void Board::delete_page(int id) {
-    print_board;
+    print_board();
 }
 
 void Board::modify_content(int id, char content) {
-    print_board;
+    print_board();
 
 }
 void Board::modify_position(int id, int x, int y) {
-    print_board;
+    print_board();
     
 }
